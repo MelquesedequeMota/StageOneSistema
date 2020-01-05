c316ea8d59546e945c4ca3a35e75985c0089010a
Vcnt Ferreira <vicentedpferreira@outlook.com>
2020-01-05 20:26:51 -0300

    home iniciado

---
+++ resources/views/home.blade.php
@@ -0,0 +1,46 @@
+@extends('master')
+
+@section('title', 'Home')
+
+@section('content')
+
+<div class="comparativoVendas">
+	<h4 class="suasVendas">
+		Suas<br>Vendas
+	</h4>
+	<div class="boxVenda">
+		<div class="boxContent">
+			<div class="valorVenda">100</div>
+			<div class="periodoVenda">Hoje</div>
+		</div>
+	</div>
+	<div class="boxVenda">
+		<div class="boxContent">
+			<div class="valorVenda">300</div>
+			<div class="periodoVenda">Ontem</div>
+		</div>
+	</div><div class="boxVenda">
+		<div class="boxContent">
+			<div class="valorVenda">900</div>
+			<div class="periodoVenda">Esse Mês</div>
+		</div>
+	</div><div class="boxVenda">
+		<div class="boxContent">
+			<div class="valorVenda">1500</div>
+			<div class="periodoVenda">Esse Ano</div>
+		</div>
+	</div>
+
+	
+</div>
+
+<div class="graficoVendas">
+	
+</div>
+
+<div class="produtosEstoque">
+	
+</div>
+
+
+@endsection
